0bf3dbcd1ab872644a4e4f2295325582fef52a7b
David Rafe <david.rafe@gmail.com>
2021-06-07 19:08:24 -0400

    commit tree c8c2fd72f9444fdee035c6a3cedf574586bcc7f7

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    let firstDrivers = drivers.filter(driver => {
+        if(driver === drivers[0] || driver === drivers[1]){
+            return driver 
+        }
+    })
+    return firstDrivers
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    let lastDrivers = drivers.filter(driver => {
+        if(driver === drivers[2] || driver === drivers[3]){
+            return driver 
+        }
+    })
+    return lastDrivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(int) {
+    return function(fare) {
+       return fare * int 
+    }
+}
+
+
+function fareDoubler(fare) {
+    return fare * 2
+}
+
+function fareTripler(fare) {
+    return fare * 3
+}
+
+function selectDifferentDrivers(drivers, func) {
+    if(func === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers)
+    } else if (func === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(drivers)
+    }
+}
